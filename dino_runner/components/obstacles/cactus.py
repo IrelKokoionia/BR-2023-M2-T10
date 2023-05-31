c393612f1cd0c4dd37c94b60c648582f0008e7b6
Felipe Atila <133978049+IrelKokoionia@users.noreply.github.com>
2023-05-31 13:28:26 -0300

    atividade aula 2
    adição do large_cactus e criação e adição de bird.py

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -1,10 +1,17 @@
 import random
 
 from dino_runner.components.obstacles.obstacle import Obstacle
-
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
 
 class Cactus(Obstacle):
-    def __init__(self, image):
+
+    CACTUS = [
+        (SMALL_CACTUS, 325),
+        (LARGE_CACTUS, 300)
+    ]
+
+    def __init__(self):
+        image, cactus_pos = self.CACTUS[random.randint(0, 1)]
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
-        self.rect.y = 325
+        self.rect.y = cactus_pos
--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -13,6 +13,7 @@ class Obstacle(Sprite):
 
     def update(self, game_speed, obstacles):
         self.rect.x -= game_speed
+        
         if self.rect.x < -self.rect.width:
             obstacles.pop()
 

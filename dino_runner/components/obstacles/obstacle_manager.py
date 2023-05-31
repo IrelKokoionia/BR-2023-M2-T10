df1d374d53347087e19e5e7074dbffcaf929dd62
Felipe Atila <133978049+IrelKokoionia@users.noreply.github.com>
2023-05-31 20:24:43 -0300

    aula 4- comit
    modificações em main.py obstacle_managar.py e game.py

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -4,6 +4,7 @@ import random
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.components.obstacles.bird import Bird
 
+
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
@@ -11,19 +12,22 @@ class ObstacleManager:
     def update(self, game):
         obstacle_type = [
             Cactus(),
-            Bird()
+            Bird(),
         ]
-
         if len(self.obstacles) == 0:
-            self.obstacles.append(obstacle_type[random.randint(0, 1)])
+            self.obstacles.append(obstacle_type[random.randint(0,1)])
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1
                 break               
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_obstacles(self):
+        self.obstacles = []

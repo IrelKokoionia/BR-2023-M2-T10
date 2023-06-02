58643a41ee3f5d99176557d9d0f3bceb48a87abc
Felipe Atila <133978049+IrelKokoionia@users.noreply.github.com>
2023-06-01 21:44:36 -0300

    Commit do código da aula 5
    codigo da aula- power_ups manager e .py

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -3,12 +3,12 @@ import random
 
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.components.obstacles.bird import Bird
+from dino_runner.components.power_ups.Power_up_manager import PowerUpManager
 
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
-
     def update(self, game):
         obstacle_type = [
             Cactus(),
@@ -16,18 +16,19 @@ class ObstacleManager:
         ]
         if len(self.obstacles) == 0:
             self.obstacles.append(obstacle_type[random.randint(0,1)])
-
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
-            if game.player.dino_rect.colliderect(obstacle.rect):
-                pygame.time.delay(500)
-                game.playing = False
-                game.death_count += 1
-                break               
+            if game.player.dino_rect.colliderect(obstacle.rect):          
+                if not game.player.has_power_up:
+                    pygame.time.delay(500)
+                    game.playing = False
+                    game.death_count += 1
+                    break
+                else:
+                    self.obstacles.remove(obstacle)               
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
-
     def reset_obstacles(self):
         self.obstacles = []
